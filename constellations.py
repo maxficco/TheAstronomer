3236db14a0267db696a79b4c390bf22636f11a9b
Max Ficco <maxwell.ficco@gmail.com>
2021-01-03 21:29:52 -0700

    sublime :)

--- constellations.py
+++ constellations.py
@@ -1,1 +1,149 @@
-print("Wish me luck! :)") 
+import pygame, sys
+
+clock = pygame.time.Clock()
+pygame.init()
+pygame.display.set_caption("Constellations")
+WINDOW_SIZE = (1000, 600)
+screen = pygame.display.set_mode(WINDOW_SIZE)
+display = pygame.Surface((500,300))
+
+gameRunning = True
+
+tile1 = pygame.image.load("tile1.png")
+tile2 = pygame.image.load("tile2.png")
+tile5 = pygame.image.load("tile5.png")
+tile6 = pygame.image.load("tile6.png")
+sprite1 = pygame.image.load("sprite1.png")
+sprite2 = pygame.image.load("sprite2.png")
+spritebox = pygame.Rect(1500,500, 12,16)
+mainframe = sprite1
+
+def load_map():
+    f = open("map.txt","r")
+    data = f.read()
+    f.close()
+    data = data.split("\n")
+    game_map = []
+    for row in data:
+        game_map.append(list(row))
+    return game_map
+game_map = load_map()
+
+moving_left = False
+moving_right = False
+vertical_momentum = 0
+airtimer = 0
+
+scroll = [0,0]
+
+def test_collisions(rect, tiles):
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+
+
+def move(rect, movement, tiles, vertical_momentum):
+    collision_types = {"top": False, "bottom": False, "left": False, "right": False}
+    rect.x += movement[0]
+    hit_list = test_collisions(rect, tiles)
+    for tile in hit_list:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collision_types["right"] = True 
+        if movement[0] < 0:
+            rect.left = tile.right
+            collision_types["left"] = True
+    rect.y += movement[1]
+    hit_list = test_collisions(rect, tiles)
+    for tile in hit_list:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types["bottom"] = True
+        if movement[1] < 0:
+            rect.top = tile.bottom
+            collision_types["top"] = True
+            vertical_momentum = 0
+    return rect, collision_types, vertical_momentum
+
+while gameRunning:
+# Reset display surface to gray
+    display.fill((115,114,119))
+
+    scrollchangex = int((spritebox.x-scroll[0]-258)/20)
+    scrollchangey = int((spritebox.y-scroll[1]-158)/20)
+    scroll[0] += scrollchangex
+    scroll[1] += scrollchangey
+    
+# Pygame Events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameRunning = False
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                moving_left = True
+                mainframe = sprite1
+            if event.key == pygame.K_RIGHT:
+                moving_right = True
+                mainframe = sprite2
+            if event.key == pygame.K_UP:
+                if airtimer < 10:
+                    vertical_momentum = -3.125
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moving_left = False
+            if event.key == pygame.K_RIGHT:
+                moving_right = False
+    
+# Draw Tiles and Map Rects
+    tile_rects = []
+    y = 0
+    for row in game_map:
+        x = 0
+        for tile in row:
+            if tile == "1":
+                display.blit(tile1, (x*16-scroll[0],y*16-scroll[1]))
+            if tile == "2":
+                display.blit(tile2, (x*16-scroll[0],y*16-scroll[1]))
+            if tile == "5":
+                display.blit(tile5, (x*16-scroll[0],y*16-scroll[1]))
+            if tile == "6":
+                display.blit(tile6, (x*16-scroll[0],y*16-scroll[1]))
+            if tile != "0":
+                tile_rects.append(pygame.Rect(x*16,y*16, 16,16))
+            x += 1
+        y += 1
+    
+# Player Movement
+    player_movement = [0,0]
+    if moving_left == True:
+        player_movement[0] -= 1
+    if moving_right == True:
+        player_movement[0] += 1
+    # gravity
+    player_movement[1] += vertical_momentum
+    vertical_momentum += 0.125
+    if vertical_momentum > 3:
+        vertical_momentum = 3
+
+
+    spritebox, collision_types, vertical_momentum = move(spritebox, player_movement, tile_rects, vertical_momentum)
+    
+    if collision_types["bottom"] == True:
+        vertical_momentum = 0
+        airtimer = 0
+    else:
+        airtimer += 1
+
+    display.blit(mainframe, (spritebox.x-scroll[0], spritebox.y-scroll[1]))
+    
+# Draw and update display
+    screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0,0))
+    pygame.display.update()
+    print(clock.get_fps())
+# Time Delay
+    clock.tick(120)
+    print(clock.get_fps())
